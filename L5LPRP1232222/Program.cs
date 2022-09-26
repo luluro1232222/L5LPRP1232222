5f7bfa69f19488fbd244ca2f4025869c551a0cd8
Lucia <Lucia@DESKTOP-VLHNU2L>
2022-09-26 13:41:25 -0600

    Agregar archivos de proyecto.

---
+++ L5LPRP1232222/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+int  numeroEntero = 0;
+
+Console.WriteLine("EJERCICIO 1");
+Console.WriteLine("Número Entero");
+
+numeroEntero = Convert.ToInt32(Console.ReadLine());
+
+if (numeroEntero >= 1)
+{
+    Console.WriteLine("Número positivo");
+}
+if (numeroEntero <= -1)
+{
+    Console.WriteLine("Numero negativo");
+}
+if (numeroEntero == 0)
+{
+    Console.WriteLine("El número que ingresó es cero");
+}
+
+int numeroDia;
+
+Console.WriteLine("EJERCICIO 2");
+Console.WriteLine("Número de día");
+
+numeroDia = Convert.ToInt32(Console.ReadLine());
+
+if (numeroDia < 0)
+
+
